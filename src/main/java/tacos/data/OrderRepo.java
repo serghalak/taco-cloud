1b1ea534933dd26df9b9b9ecf72bc6ed07323f78
serghalak <serghalak@gmail.com>
2019-05-18 17:40:23 +0300

    end of configuration

--- src/main/java/tacos/data/OrderRepo.java
+++ src/main/java/tacos/data/OrderRepo.java
@@ -1,8 +1,10 @@
 package tacos.data;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import tacos.Order;
+import tacos.User;
 
 import java.util.Date;
 import java.util.List;
@@ -11,6 +13,8 @@ public interface OrderRepo extends CrudRepository<Order,Long> {
     List<Order> findByDeliveryZip(String deliveryZip);
     List<Order> readOrdersByDeliveryZipAndPlaceAtBetween(
             String deliveryZip, Date startDate, Date endDate);
+
+    List<Order> findByUserOrderByPlaceAtDesc(User user, Pageable pageable);
 
 //    @Query("Order o where o.city='Seattle'")
 //    List<Order> readOrdersDeliveredInSeattle();
--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -1,7 +1,8 @@
 package tacos.web;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,18 +16,31 @@ import tacos.data.OrderRepo;
 import tacos.data.UserRepository;
 
 import javax.validation.Valid;
-import java.security.Principal;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("order")
+//@ConfigurationProperties(prefix = "taco.orders")
 public class OrderController {
 
+//    private int pageSize=20;
+//
+//    public int getPageSize() {
+//        return pageSize;
+//    }
+//
+//    public void setPageSize(int pageSize) {
+//        this.pageSize = pageSize;
+//    }
+    private final OrderProps orderProps;
     private final OrderRepo orderRepository;
     private final UserRepository userRepository;
 
-    public OrderController(OrderRepo orderRepository,UserRepository userRepository) {
+    public OrderController( OrderRepo orderRepository
+            , UserRepository userRepository
+            , OrderProps orderProps) {
+        this.orderProps = orderProps;
         this.orderRepository = orderRepository;
         this.userRepository=userRepository;
     }
@@ -62,5 +76,23 @@ public class OrderController {
         orderRepository.save(order);
         sessionStatus.setComplete();
         return "redirect:/";
+    }
+
+//    @GetMapping
+//    public String ordersForUser(@AuthenticationPrincipal User user
+//            ,Model model){
+//        model.addAttribute("orders"
+//                ,orderRepository.findByUserOrderByPlaceAtDesc(user));
+//        return "orderList";
+//    }
+
+    @GetMapping
+    public String ordersForUser(@AuthenticationPrincipal User user
+            ,Model model){
+        System.out.println("pageSize: " + orderProps.getPageSize());
+        Pageable pageable= PageRequest.of(0,orderProps.getPageSize());
+        model.addAttribute("orders",
+                orderRepository.findByUserOrderByPlaceAtDesc(user,pageable));
+        return "orderList";
     }
 }
